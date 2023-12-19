372a7e382ba9163b7d74ace420497fa1e1ea9a2e
Praful Nigam <prafulnigam16@gmail.com>
2023-12-20 00:11:25 +0530

    Worked with Passport & Passport JWT

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require("dotenv").config();
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const User = require("./models/User")
 const app = express();
 const port = 8000;
 
@@ -21,6 +25,27 @@ mongoose.connect("mongodb+srv://prafulnigam16:" + process.env.MONGO_PASSWORD + "
     console.log("Error while connecting to Mongo");
 });
 
+// setup of passport-jwt
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET_KEY; // this key would be in env 
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        // does the user exist ?? 
+
+        //if error then return false
+        if (err) {
+            return done(err, false);
+        }
+        // if exist return the user
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
 
 app.get("/", (req, res) => {
     // req contains all data for the request 

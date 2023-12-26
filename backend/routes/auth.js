15ab2d32f58869db8e41579befb59110b91ce2c7
Praful Nigam <prafulnigam16@gmail.com>
2023-12-26 16:35:28 +0530

    Worked over /login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,12 +8,14 @@ router.post("/register", async (req, res) => {
   try {
     const { email, password, firstName, lastName, username } = req.body;
 
-    // // Check if user exists
-    // const user = await User.findOne({ email: email });
+    // Check if user exists
+    const user = await User.findOne({ email: email });
 
-    // if (user) {
-    //   return res.status(403).json({ error: "A user with this email already exists" });
-    // }
+    if (user) {
+      return res
+        .status(403)
+        .json({ error: "A user with this email already exists" });
+    }
 
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -31,7 +33,7 @@ router.post("/register", async (req, res) => {
 
     // Create a token and return it to the user
     const token = await getToken(email, newUser);
-    
+
     // Return user data without the password
     const userToReturn = { ...newUser.toJSON(), token };
     delete userToReturn.password;
@@ -41,6 +43,21 @@ router.post("/register", async (req, res) => {
     console.error("Error during registration:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
+});
+
+router.post("/login", async (req,res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email : email});
+    if(!user) return res.status(403).json({err: "Inavlid Cridentials"});
+
+    // now we will check hash == hash 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid) return res.status(403).json({err: "Inavlid Cridentials"});
+
+    const token = await getToken(user.email, user);
+    const userToReturn = { ...user.toJSON(), token };
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
 });
 
 module.exports = router;
